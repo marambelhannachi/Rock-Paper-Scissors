ac84cbeeb2e0b08fe1d2a0461bc93ee5c033d7f9
marambelhannachi <maramexx3@gmail.com>
2025-11-09 16:43:05 +0100

    use arrow function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let isAutoPlaying = false;
 let intervalId;
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
